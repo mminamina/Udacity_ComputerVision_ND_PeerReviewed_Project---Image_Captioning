3f76d760884bf221e8eb6fe11070a759ba21cb63
Mina M <59129933+mminamina@users.noreply.github.com>
2019-12-21 23:08:30 -0800

    Project 2 files uploaded

---
+++ model.py
@@ -0,0 +1,54 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+
+
+class EncoderCNN(nn.Module):
+    def __init__(self, embed_size):
+        super(EncoderCNN, self).__init__()
+        resnet = models.resnet50(pretrained=True)
+        for param in resnet.parameters():
+            param.requires_grad_(False)
+        
+        modules = list(resnet.children())[:-1]
+        self.resnet = nn.Sequential(*modules)
+        self.embed = nn.Linear(resnet.fc.in_features, embed_size)
+
+    def forward(self, images):
+        features = self.resnet(images)
+        features = features.view(features.size(0), -1)
+        features = self.embed(features)
+        return features
+    
+
+class DecoderRNN(nn.Module):
+    def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
+        super(DecoderRNN, self).__init__()
+        self.embed_size = embed_size
+        self.hidden_size = hidden_size
+        self.vocab_size = vocab_size
+        self.embeddings = nn.Embedding(vocab_size, embed_size)
+        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size, vocab_size)
+    
+    def forward(self, features, captions):
+        caption_embeds = self.embeddings(captions[:, :-1])
+        features = features.unsqueeze(1)
+        inputs = torch.cat((features, caption_embeds), 1)
+        hidden, states = self.lstm(inputs)
+        output = self.fc(hidden)
+        return output
+
+    def sample(self, inputs, states=None, max_len=20):
+        " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
+        tokens = []
+        for i in range(max_len):
+            hidden, states = self.lstm(inputs, states)
+            outputs = self.fc(hidden.squeeze(1))
+            index = outputs.argmax(1)
+            word = index.item()
+            tokens.append(word)
+            if word == 1:
+                break
+            inputs = self.embeddings(index.unsqueeze(1))
+        return tokens
